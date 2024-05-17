003f147bc0ac8bffbd7c1482f70966dcdc3ed2ed
Harshada <hyadav8701@conestogac.on.ca>
2024-05-17 19:32:47 -0400

    getting user data

--- Controller/User.js
+++ Controller/User.js
@@ -39,3 +39,18 @@ exports.loginUser = async (req, res) => {
     res.status(500).send("Internal server error");
   }
 };
+
+exports.getUser = async (req, res) => {
+  try {
+    const { email } = req.params;
+    console.log("req", req);
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.json(user);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server Error" });
+  }
+};
